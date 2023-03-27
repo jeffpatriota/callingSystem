8d1957ae9aae90ed7f20d5b0cba6341d5b58082d
jeffpatriota <jefferson.patriota@outlook.com>
2023-03-27 18:00:47 -0300

    create routes

---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+import { Routes, Route } from 'react-router-dom'
+
+import SignIn from '../pages/SignIn'
+import SignUp from '../pages/SignUp'
+
+function RoutesApp() {
+    return(
+        <Routes>
+            <Route path='/' element={<SignIn/>}/>
+            <Route path='/Register' element={<SignUp/>}/>
+        </Routes>
+    )
+}
+
+export default RoutesApp;
