c5bf82261f3054c20bcd21e6327f2cf183931bf4
JEFFERSON ALVES PATRIOTA <jefferson.patriota@hcfmusp-pa.phcnet.usp.br>
2023-05-02 15:23:14 -0300

    logging user

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -9,7 +9,7 @@ export default function SignIn() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    const { signIn } = useContext(AuthContext)
+    const { signIn, loadingAuth } = useContext(AuthContext)
 
     function handleSignIn(e) {
         e.preventDefault();
@@ -36,7 +36,10 @@ export default function SignIn() {
                             value={password}
                             onChange={(e) => setPassword(e.target.value)} />
 
-                        <button type='submit'> Acessar </button>
+                        
+                    <button type='submit'>
+                        {loadingAuth ? 'Carregando...' : 'Acessar'}
+                    </button>
                     </form>
 
                     <Link to="/register">Criar uma conta</Link>
