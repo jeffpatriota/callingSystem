16eeca9773f74d109f95eae337669c5283720bce
Jefferson Patriota <jefferson.patriota@outlook.com>
2023-06-13 17:11:24 -0300

    new call page

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -1,20 +1,68 @@
-import {useState} from 'react'
+import {useState, useEffect, useContext} from 'react'
 import Header from '../../components/Header'
 import Title from '../../components/Title'
 import { FiPlusCircle } from 'react-icons/fi'
 
+import { AuthContext } from '../../contexts/auth'
+import { db } from '../../services/firebaseConnection' 
+import { collection, getDocs, getDoc, doc } from 'firebase/firestore'
+
 import './style.css';
+
+const lisRef = collection(db, "customers")
 
 export default function New() {
 
     const [customers, setCustomers] = useState([])
+    const [loadCustomer, setLoadCostumer] = useState(true);
+    const [costumerSelected, setCostumerSelected] = useState(0);
 
     const [complemento, setComplemento] = useState('')
     const [assunto, setAssunto] = useState('Suporte')
     const [status, setStatus] = useState('Aberto')
 
+    useEffect(() =>{
+        async function loadCustomer(){
+            const querySnapshot = await getDocs(lisRef)
+        .then((snapshot)=>{
+            let lista = [];
+
+            snapshot.forEach((doc)=>{
+                lista.push({
+                    id: doc.id,
+                    nomeFantasia: doc.data().nomeFantasia
+                })
+            })
+
+            if(snapshot.docs.size === 0){
+                console.log("NENHUMA EMPRESA ENCONTRADA");
+                setCustomers([ {id: '1', nomeFantasia: 'FREELA'} ]);
+                setLoadCostumer(false);
+                return;
+            }
+
+            setCustomers(lista);
+            setLoadCostumer(false)
+
+        })
+        .catch((error)=>{
+            console.log("ERRO AO BUSCAR OS CLIENTES", error)
+            setLoadCostumer(false);
+            setCustomers([{id: '1', nomeFantasia: 'FREELA'}])
+        })
+        }
+
+        loadCustomer();
+    },[])
+
+
     function handleOptionChange(e){
         setStatus(e.target.value);
+    }
+
+    function handleChangeSelect(e){
+        setCostumerSelected(e.target.value)
+        console.log(customers[e.target.value].nomeFantasia);
     }
 
     return (
@@ -30,10 +78,21 @@ export default function New() {
                     <form className="form-profile">
 
                         <label>Clientes</label>
-                        <select>
-                            <option key={1} value={1}>Tech Ninja</option>
-                            <option key={2} value={2}>Solar View</option>
-                        </select>
+                       {
+                        loadCustomer ? (
+                            <input type="text" disabled ={true} value="Carregando..."/>
+                        ) : (
+                            <select value={costumerSelected} onChange={handleChangeSelect}>
+                                {customers.map((item, index) => {
+                                    return(
+                                        <option key={index} value={index}>
+                                        {item.nomeFantasia}
+                                        </option>
+                                    )
+                                })}
+                            </select>
+                        )
+                       }
 
                         <label>Assunto</label>
                         <select>
